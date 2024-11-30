04377b1ffb7c60dfc610d008f21884231a3054ce
Juhi-Shah06 <shahjuhi0606@gmail.com>
2024-11-30 10:35:51 +0530

    Initial commit

--- Sorting/Recursive_BubbleSort.cpp
+++ Sorting/Recursive_BubbleSort.cpp
@@ -1,11 +1,42 @@
-/*Write a program to implement bubble sort by using recursion.
-Time complexity: O()*/
+// Bubble Sort using Recursion
+// Time Complexity: O(N^2)
+// Space complexity: O(N)
 
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+void bubbleSortRecursive(vector<int> &arr, int n)
+{
+    if (n == 1)
+        return;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            swap(arr[i], arr[i + 1]);
+        }
+    }
+    bubbleSortRecursive(arr, n - 1);
+}
 
 int main()
 {
     int n;
-    
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter the elements:\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubbleSortRecursive(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
